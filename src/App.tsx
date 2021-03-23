60af0ddbf83ff34fd715d857354a94a65d6f3217
Nelson Liao <shenlnelson9257@outlook.com>
2021-03-23 11:24:43 +0800

    first commit for typescript two

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,54 @@
 import './App.css';
 import React, { Component } from 'react';
 
-interface stateProps {
+// interface addProps {
+//     value1: number,
+//     value2: number
+// }
+//
+// interface paragraphProps {
+//     content: string
+// }
+//
+// interface stateProps extends addProps, paragraphProps{
+// }
+
+type addProps = {
     value1: number,
     value2: number
 }
+
+type paragraphProps = {
+    content: string
+}
+
+type stateProps = addProps & paragraphProps;
 
 class App extends Component {
   state: stateProps = {
       value1: 1,
       value2: 2,
+      content: 'Hello, this is a calculator',
   }
 
-  addValue(value1: number, value2: number) {
-    return value1 + value2;
-  }
+    addValue = (numbers: addProps) => {
+        return numbers.value1 + numbers.value2;
+    }
+
+    addParagraph = (paragraph: paragraphProps) => {
+        return paragraph.content;
+    }
 
   render () {
-        const { value1, value2 } = this.state;
+        const { value1, value2, content } = this.state;
+        const values = {value1, value2};
+        const paragraph = { content };
         return (
             <div className="app">
                 <p>The first value is {value1}</p>
                 <p>The second value is {value2}</p>
-                <p>When they add together, it equals to {this.addValue(value1, value2)}</p>
+                <p>When they add together, it equals to {this.addValue(values)}</p>
+                <p>{this.addParagraph(paragraph)}</p>
             </div>
         );
   }
